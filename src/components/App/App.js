ddcf35f8206960b46236575da2ea5fc6f2aa4e8c
Zane Kuehn <zanekuehn@yahoo.com>
2019-09-17 14:17:05 -0500

    Changes to Registration Form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Provider } from 'react-redux';
-
-
+import{Route,Link} from 'react-router-dom';
 import Rides from '../Rides/Rides'
 import Header from '../Header/Header';
-
 import store from '../../store';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons'
+import RegistrationForm from '../RegistrationForm/RegistrationForm';
+import LoginForm from '../LoginForm/LoginForm';
+import CreateRideForm from '../RegistrationForm/CreateRideForm/CreateRideForm';
 
 library.add(fas, faMapMarkedAlt)
 
@@ -19,7 +20,10 @@ class App extends Component {
       <Provider store={store}>
         <div className="App">
           <Header />
-          <Rides />
+          <Route exact path='/' component={RegistrationForm}/>
+          <Route path='/login'  component={LoginForm}/>
+          <Route path='/rides'component={Rides} />
+          <Route path='/createride' component={CreateRideForm}></Route>
         </div>
       </Provider>
     )
