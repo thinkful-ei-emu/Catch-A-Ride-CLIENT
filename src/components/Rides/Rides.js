d38990f755867536ea5f7513265c367488cc9b93
Jvymvn <Jordon.carter@yahoo.com>
2019-09-17 15:33:05 -0400

    Api service method update

--- src/components/Rides/Rides.js
+++ src/components/Rides/Rides.js
@@ -9,14 +9,17 @@ class Rides extends Component {
     static contextType = RideContext;
 
     componentDidMount() {
+        const starting = this.context.s
         RideApiService.getRides()
             .then(res => {
                 this.context.setRides(res);
             })
-
     }
 
     renderRidesList() {
+        const { searchTerm, filterOption } = this.context;
+        const list = this.context.rides.filter(ride => (ride.destination.includes(searchTerm)
+            && (filterOption === 'destination'))).map((ride, key) => <li key={key}><Ride ride={ride} /></li>)
         const ridePosts = this.context.rides.map((ride, i) => (
             <li key={i}><Ride ride={ride} /></li>
         ))
