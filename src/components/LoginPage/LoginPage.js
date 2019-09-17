b0759188acfbb5332cc0c7450a7e9a35b3bec88a
Andrew <andrewyin1994@gmail.com>
2019-09-17 12:35:25 -0700

    google auth token verification seems to be working

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { GoogleLogin, GoogleLogout } from 'react-google-login';
 
+import config from '../../config';
+
 class LoginPage extends React.Component {
   constructor(props) {
     super(props);
@@ -15,16 +17,38 @@ class LoginPage extends React.Component {
     script.src = './js/script.js';
   }
 
-  onSignIn = (googleUser) => {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  onSignIn = async (googleUser) => {
+    const profile = googleUser.getBasicProfile();
+    const id_token = googleUser.getAuthResponse().id_token;
 
-    this.setState({
-      loggedIn: true
-    });
+    console.log(id_token);
+
+    // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    // console.log('Name: ' + profile.getName());
+    // console.log('Image URL: ' + profile.getImageUrl());
+    // console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+    try {
+      let stuff = await fetch(config.API_ENDPOINT + '/auth/glogin', {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          id_token
+        })
+      });
+      stuff = await stuff.json();
+
+      console.log(stuff);
+
+      this.setState({
+        loggedIn: true
+      });
+    }
+    catch (e) {
+      console.error(e.message);
+    }
   }
   googleResponse = (response) => {
     console.log(response);
@@ -41,7 +65,7 @@ class LoginPage extends React.Component {
   render() {
     return <>
       <GoogleLogin
-        clientId='343760859315-00sfmokq5vc50ufqa8jg35rscb307eb5.apps.googleusercontent.com'
+        clientId={config.CLIENT_ID}
         buttonText={this.state.loggedIn ? 'Signed In' : 'Sign In'}
         onSuccess={this.onSignIn}
         onFailure={this.googleResponse}
@@ -50,7 +74,7 @@ class LoginPage extends React.Component {
       </GoogleLogin>
 
       <GoogleLogout
-        clientId='343760859315-00sfmokq5vc50ufqa8jg35rscb307eb5.apps.googleusercontent.com'
+        clientId={config.CLIENT_ID}
         buttonText='Sign Out'
         onLogoutSuccess={this.logout}
       />
---
+++ src/config.js
@@ -0,0 +1,6 @@
+const config = {
+  CLIENT_ID: process.env.REACT_APP_CLIENT_ID,
+  API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8080/api'
+};
+
+export default config;
