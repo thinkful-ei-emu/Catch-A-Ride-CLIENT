3057be87567f444a71d4ed498f11968e3082c386
Zane Kuehn <zanekuehn@yahoo.com>
2019-09-17 10:51:12 -0500

    Reg. Form Component

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -1,2 +1,21 @@
-import React, { Component } from 'react';
+import React from "react";
 
+export default class RegistrationForm extends React.Component {
+  render() {
+    return (
+      <section>
+        <form className="registration-form">
+          <label>Enter Full Name</label>
+          <input placeholder="Enter First and Last Name"></input>
+          <label>Email:</label>
+          <input placeholder="Enter Email"></input>
+          <label>UserName:</label>
+          <input placeholder="Enter deired username "></input>
+          <label>Password:</label>
+          <input placeholder="Enter Password"></input>
+          <button type="submit">Register</button>
+        </form>
+      </section>
+    );
+  }
+}
