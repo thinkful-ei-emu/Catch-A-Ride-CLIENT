032cb54caecb5c3db3091e2fc3efd3872619cb65
Adrienne Smith <agsmith046@gmail.com>
2019-09-17 16:05:01 -0500

    fixed route issue

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,7 @@ import Rides from '../Rides/Rides';
 import Header from '../Header/Header';
 import RideSearchBar from '../RideSearchBar/RideSearchBar';
 
+import{Route, Link} from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons'
@@ -19,9 +20,10 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <RideSearchBar />
-        <hr />
-        <Rides />
+        <Route exact path='/' component={RegistrationForm}/>
+        <Route path='/login'  component={LoginForm}/>
+        <Route path='/rides'component={Rides} />
+        <Route path='/createride' component={CreateRideForm}></Route>
       </div>
     )
   }
