d06be29110b2bd83e4c5d40e537db06220a22667
Adrienne Smith <agsmith046@gmail.com>
2019-09-19 15:47:53 -0500

    added smoke tests

--- src/tests/Ride.test.js
+++ src/tests/Ride.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {MemoryRouter} from 'react-router-dom';
+import Ride from '../components/Ride/Ride';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<MemoryRouter><Ride /></MemoryRouter>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
