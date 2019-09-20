3d78b23b8749058eed1172b206e88771b6408ea2
Zane Kuehn <zanekuehn@yahoo.com>
2019-09-20 16:46:47 -0500

    fix

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'https://catch-a-ride-39963.herokuapp.com/',
+  API_ENDPOINT: 'https://catch-a-ride-39963.herokuapp.com/api',
   GMAPS_API_KEY: process.env.REACT_APP_GMAPS_API_KEY,
   GEO_API_KEY: process.env.REACT_APP_GEO_API_KEY,
   CLIENT_ID: process.env.REACT_APP_CLIENT_ID,
