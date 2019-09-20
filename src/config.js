50bc79392d8a5bb1fc64369cab07ae5dd80f7b7f
Zane Kuehn <zanekuehn@yahoo.com>
2019-09-20 16:36:48 -0500

    Backend hookup

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8080/api',
+  API_ENDPOINT: 'https://catch-a-ride-39963.herokuapp.com/',
   GMAPS_API_KEY: process.env.REACT_APP_GMAPS_API_KEY,
   GEO_API_KEY: process.env.REACT_APP_GEO_API_KEY,
   CLIENT_ID: process.env.REACT_APP_CLIENT_ID,
