42bf80ee91fa709e868ab326ff45cab0e23080d1
Jvymvn <Jordon.carter@yahoo.com>
2019-09-20 14:58:57 -0400

    merge

--- src/services/RidesService/rides-passenger-service.js
+++ src/services/RidesService/rides-passenger-service.js
@@ -37,7 +37,7 @@ const RidesApiService = {
     })
       .then(res => {
         (!res.ok)
-          ? res.json.then(e => Promise.reject(e))
+          ? res.json().then(e => Promise.reject(e))
           : res.json()
       })
   },
