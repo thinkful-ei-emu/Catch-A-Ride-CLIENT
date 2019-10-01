cb33b517072647d909b60f5af935ce86cf90deaf
Adrienne Smith <agsmith046@gmail.com>
2019-10-01 09:13:38 -0500

    added cypress, started test for adding ride

---
+++ cypress/integration/adding-ride.spec.js
@@ -0,0 +1,27 @@
+/// <reference types="Cypress" />
+
+describe('Driver can add a ride', function () {
+  beforeEach(() => {
+    cy.login().visit('/')
+  });
+
+  context('Before submit', () => {
+    it('shows form', () => {
+      cy.visit('/createride');
+      cy.get('h2')
+        .should('contain', 'Create Ride');
+      cy.get('form')
+        .should('contain', 'label', 'Starting Point')
+        .should('contain', 'label', 'Destination')
+        .should('contain', 'label', 'Date')
+        .should('contain', 'label', 'Time')
+        .should('contain', 'label', 'Description')
+        .should('contain', 'label', 'Capacity');
+    });
+  });
+
+  // context('After submit', () => {
+
+  // });
+
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,13 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', () => {
+  cy.visit('/login')
+    .window()
+    .then(win => {
+      win.localStorage.setItem(
+        Cypress.env('')
+      );
+    });
+});
