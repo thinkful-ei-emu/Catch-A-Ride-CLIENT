7b0e9218ef268d19c80cd82afeb5db37fae0dbea
Adrienne Smith <agsmith046@gmail.com>
2019-10-02 12:16:12 -0500

    finished first part of ride test

--- cypress/integration/adding-ride.spec.js
+++ cypress/integration/adding-ride.spec.js
@@ -2,21 +2,35 @@
 
 describe('Driver can add a ride', function () {
   beforeEach(() => {
-    cy.login().visit('/');
+    cy.server()
+      .route({
+        method: 'POST',
+        url: '/api/auth/glogin',
+        status: 200,
+        response: 'fixture:user'
+      })
+      .as('userRequest');
+  });
+
+  beforeEach(() => {
+    // needs to change
+    cy.login().visit('/createride');
   });
 
   context('Before submit', () => {
     it('shows form', () => {
-      cy.visit('/createride');
+      // needs to change
+      cy.request('/createride');
       cy.get('h2')
-      // .should('contain', 'Create Ride');
-      // cy.get('form')
-      //   .should('contain', 'label', 'Starting Point')
-      //   .should('contain', 'label', 'Destination')
-      //   .should('contain', 'label', 'Date')
-      //   .should('contain', 'label', 'Time')
-      //   .should('contain', 'label', 'Description')
-      //   .should('contain', 'label', 'Capacity');
+        .should('contain', 'Create Ride');
+      cy.get('form')
+        .should('contain', 'Starting Point')
+        .should('contain', 'Destination')
+        .should('contain', 'Date')
+        .should('contain', 'Time')
+        .should('contain', 'Description')
+        // needs to change
+        .should('contain', '# of Seats');
     });
   });
 
