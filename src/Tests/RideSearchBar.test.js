8848e46a3fce723923d84f5bdba808c9449543db
Adrienne Smith <agsmith046@gmail.com>
2019-10-03 12:19:37 -0500

    finished writing snapshot tests

--- src/Tests/RideSearchBar.test.js
+++ src/Tests/RideSearchBar.test.js
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {MemoryRouter} from 'react-router-dom';
 import RideSearchBar from '../components/RideSearchBar/RideSearchBar';
+import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<MemoryRouter><RideSearchBar /></MemoryRouter>, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders the UI as expected', () => {
+  const tree = renderer
+    .create(<MemoryRouter><RideSearchBar /></MemoryRouter>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
--- src/Tests/SearchBox.test.js
+++ src/Tests/SearchBox.test.js
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {MemoryRouter} from 'react-router-dom';
 import SearchBox from '../components/SearchBox/SearchBox';
+import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<MemoryRouter><SearchBox /></MemoryRouter>, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders the UI as expected', () => {
+  const tree = renderer
+    .create(<MemoryRouter><SearchBox /></MemoryRouter>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
