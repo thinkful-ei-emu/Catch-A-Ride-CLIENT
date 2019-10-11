8e1549db8968265dd6b3460baa2eac259bd2cef2
Arpan Patel <adp8ke@gmail.com>
2019-10-11 13:31:19 -0400

    added aria labels to forms and to inputs of those forms

--- src/components/EditModal/EditModal.js
+++ src/components/EditModal/EditModal.js
@@ -15,18 +15,18 @@ export default class Modal extends React.Component{
     
       return (
         <div className='modal'>
-          <form className='editForm'>
+          <form className='editForm' aria-label='Edit this ride'>
             <p>Edit Whichever Details As Needed</p>
             <label htmlFor='newStarting'>Starting</label>
-            <input type='text' id='newStarting' defaultValue={starting}></input>
+            <input type='text' id='newStarting' aria-label='Edit Starting input' defaultValue={starting}></input>
             <label htmlFor='newDestination'>Destination</label>
-            <input type='text' id='newDestination' defaultValue={destination}></input>
+            <input type='text' id='newDestination' aria-label='Edit Destination input' defaultValue={destination}></input>
             <label htmlFor='newDescription'>Description</label>
-            <textarea className='newDescription' type='text' id='newDescription' defaultValue={description}></textarea>
+            <textarea className='newDescription' type='text' id='newDescription' aria-label='Edit Description input' defaultValue={description}></textarea>
             <label htmlFor='newDate'>Date</label>
-            <input type='text' id='newDate' defaultValue={dateFormat}></input>
+            <input type='text' id='newDate' aria-label='Edit Date input' defaultValue={dateFormat}></input>
             <label htmlFor='newTime'>Time</label>
-            <input type='text' id='newTime' defaultValue={timeFormat}></input>
+            <input type='text' id='newTime' aria-label='Edit Time input' defaultValue={timeFormat}></input>
           </form>
           <button type="submit" onClick={() => handleEditForm()}>Save <FontAwesomeIcon icon={faSave} /></button>
           <button type="button" onClick={() => closeEditForm()}>Close <FontAwesomeIcon icon={faWindowClose} /></button>     
